3d005caa9738990fd799c47fba61a469a0bdd3a4
thepotatofield <nicoaubry@gmail.com>
2026-03-29 13:09:47 +0300

    Added git log alias

--- zsh/.zsh/git.zsh
+++ zsh/.zsh/git.zsh
@@ -3,5 +3,6 @@
 ############################
 alias lg='lazygit --use-config-file $DOFILES_DIR/lazygit/config.yml' # Open lazygit with custom config
 
-alias git-repo-web='open $(git config remote.origin.url);' # Open the github repo in the default web browser
-alias git-pull-all='find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;' # Pull all repositories in the current directory
+alias g-log="git log -1 --stat"
+alias g-open-url='open $(git config remote.origin.url);' # Open the github repo in the default web browser
+alias g-pull-all='find . -type d -depth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \;' # Pull all repositories in the current directory
